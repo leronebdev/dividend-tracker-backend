e97457439dd12e50b0b3fca7cebd0d7ffabb58d2
leronebdev <serverpowered@serversidedev.net>
2026-05-31 22:15:22 -0400

    initial project*

---
+++ src/main/java/com/serverside/dt/DividendTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.serverside.dt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DividendTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DividendTrackerServerApplication.class, args);
+	}
+
+}
